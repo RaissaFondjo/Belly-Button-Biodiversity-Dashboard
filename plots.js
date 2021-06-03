aedec91a8c3d09a2d097c44e5f2ab83d7df51fcb
RaissaFondjo <80492376+RaissaFondjo@users.noreply.github.com>
2021-06-03 14:57:45 -0400

    Update plots.js

--- plots.js
+++ plots.js
@@ -1,37 +1,40 @@
-// Sort the data array using the greekSearchResults value
-data.sort(function(a, b) {
-  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
-});
 
-// Slice the first 10 objects for plotting
-data = data.slice(0, 10);
+/* For example, ID "940" is the first element of the sampleNames array.
+ As the forEach() method iterates over the first element of the array,
+  a menu option is appended to the dropdown menu. It is then given the 
+  text (the text seen in the dropdown menu) "940", and its property is
+   also assigned "940". The forEach() method 
+will perform the same tasks for the next element of the array, "941".
+ */
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
 
-// Reverse the array due to Plotly's defaults
-data = data.reverse();
-
-// Trace1 for the Greek Data
-var trace1 = {
-  x: data.map(row => row.greekSearchResults),
-  y: data.map(row => row.greekName),
-  text: data.map(row => row.greekName),
-  name: "Greek",
-  type: "bar",
-  orientation: "h"
-};
-
-// data
-var data = [trace1];
-
-// Apply the group bar mode to the layout
-var layout = {
-  title: "Greek gods search results",
-  margin: {
-    l: 100,
-    r: 100,
-    t: 100,
-    b: 100
+  function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
   }
-};
-
-// Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", data, layout);
+  
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+      PANEL.append("h6").text(result.location);
+    });
+  }  
